1c61ed126ba270e56d761444168330ce11f868e9
DonaldMcRonald <raunak1997@utexas.edu>
2016-04-12 21:56:36 -0500

    better readibility

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ slack = Slacker(SLACK_API_TOKEN)
 # Get list of users in slack org
 user_list = slack.users.list().body['members']
 emails = filter(None, [u['profile']['email'] for u in user_list])
-is_admin = {u['id'] : u['is_admin'] for u in user_list}
+admin = {u['id'] : u['is_admin'] for u in user_list if u['is_admin']}
 
 sg = sendgrid.SendGridClient(SENDGRID_API_KEY)
 
@@ -26,7 +26,7 @@ def email_command(**kwargs):
 
     # check if user is admin
     user_id = kwargs.get('user_id')
-    if not is_admin[user_id] :
+    if user_id not in admin :
         return slash.response("Sorry, you must be an admin to user this command.")
 
     msg = send_email(text)
@@ -38,7 +38,7 @@ def send_email(body) :
         message = sendgrid.Mail(to=email, subject='Code Orange Update', text=body, from_email='info@codeorange.io')
         status, msg = sg.send(message)
         responses.append(status)
-    
+
     if all(response == 200 for response in responses) :
         return "Successfully sent messages."
     else :
